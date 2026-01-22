ee56ea14239c62c2481a8b88e433d964a6f7060a
ali yousefi <72976019+aliyousefi-dev@users.noreply.github.com>
2026-01-22 16:42:07 +0330

    refactor(native-player): update playpausebutton to use new playerStateService

--- web/src/app/components/media-player/native-player/controls/buttons/play-pause-button/play-pause-button.ts
+++ web/src/app/components/media-player/native-player/controls/buttons/play-pause-button/play-pause-button.ts
@@ -1,7 +1,6 @@
-// src/app/components/default-video-player/controls/play-pause-button/play-pause-button.component.ts
-
-import { Component, Input, ElementRef, OnInit } from '@angular/core';
+import { Component, Input, ElementRef, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PlayerStateService } from '../../../services/player-state.service';
 
 @Component({
   selector: 'app-play-pause-button',
@@ -10,50 +9,23 @@ import { CommonModule } from '@angular/common';
   templateUrl: './play-pause-button.html',
 })
 export class PlayPauseButton implements OnInit {
-  /**
-   * The reference to the actual <video> element from the parent component.
-   */
-  @Input({ required: true }) videoRef!: ElementRef<HTMLVideoElement>;
-
   // Local state to track if the video is currently playing/paused
   public isPlaying: boolean = false;
-  private video!: HTMLVideoElement;
+  private playerState = inject(PlayerStateService);
 
   ngOnInit() {
-    this.video = this.videoRef.nativeElement;
-
-    // Initial check
-    this.isPlaying = !this.video.paused;
-
-    // We must listen for play/pause events directly on the video element
-    // because the state can be changed by other means (e.g., clicking the video itself).
-    this.video.addEventListener('play', this.updateState);
-    this.video.addEventListener('pause', this.updateState);
+    this.playerState.isPlaying$.subscribe((p) => {
+      this.isPlaying = p;
+    });
   }
 
-  ngOnDestroy() {
-    // Clean up event listeners
-    this.video.removeEventListener('play', this.updateState);
-    this.video.removeEventListener('pause', this.updateState);
-  }
+  ngOnDestroy() {}
 
-  /**
-   * Toggles the play/pause state of the video element.
-   * This is called directly from the component's template.
-   */
   togglePlay() {
-    if (this.video.paused) {
-      this.video.play();
+    if (this.playerState.isPlaying$.value) {
+      this.playerState.pause();
     } else {
-      this.video.pause();
+      this.playerState.play();
     }
-    // Note: The state (isPlaying) is automatically updated by the event listeners.
   }
-
-  /**
-   * Updates the local isPlaying state based on video events.
-   */
-  private updateState = () => {
-    this.isPlaying = !this.video.paused;
-  };
 }
