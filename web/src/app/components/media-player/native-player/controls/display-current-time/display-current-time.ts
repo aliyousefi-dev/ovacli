6335c28ac407c49988b1e32f21f0cf68288339c4
ali yousefi <72976019+aliyousefi-dev@users.noreply.github.com>
2026-01-22 16:29:19 +0330

    refactor(native-player): update display-current-time to use new playerStateService

--- web/src/app/components/media-player/native-player/controls/display-current-time/display-current-time.ts
+++ web/src/app/components/media-player/native-player/controls/display-current-time/display-current-time.ts
@@ -1,7 +1,8 @@
 // src/app/components/media-player/native-player/controls/display-current-time/display-current-time.component.ts
-import { Component, Input, ElementRef, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { formatTime } from '../../utils/time-utils';
+import { PlayerStateService } from '../../services/player-state.service';
 
 @Component({
   selector: 'app-display-current-time',
@@ -10,28 +11,17 @@ import { formatTime } from '../../utils/time-utils';
   templateUrl: './display-current-time.html',
 })
 export class DisplayCurrentTime implements OnInit, OnDestroy {
-  @Input({ required: true }) videoRef!: ElementRef<HTMLVideoElement>;
+  playerState = inject(PlayerStateService);
 
-  public currentTime: number = 0;
+  public currentTime: string = '00';
   private video!: HTMLVideoElement;
   formatTime = formatTime;
 
   ngOnInit() {
-    if (this.videoRef && this.videoRef.nativeElement) {
-      this.video = this.videoRef.nativeElement;
-      this.video.addEventListener('timeupdate', this.updateTime);
-      // Initialize current time in case the video is already playing/seeked
-      this.updateTime();
-    }
+    this.playerState.currentTime$.subscribe((t) => {
+      this.currentTime = formatTime(t);
+    });
   }
 
-  ngOnDestroy() {
-    if (this.video) {
-      this.video.removeEventListener('timeupdate', this.updateTime);
-    }
-  }
-
-  private updateTime = () => {
-    this.currentTime = this.video?.currentTime ?? 0;
-  };
+  ngOnDestroy() {}
 }
